ff60be33570a51430535c1d5711c3714195ec878
Diogo Machado <diogo.smac@gmail.com>
2019-11-30 15:52:27 +0000

    Basic movement

---
+++ src/Gaja.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+
+const MOVE_SPEED = 250
+const TURN_ANGLE = PI / 64
+
+# Declare member variables here. Examples:
+var angle = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func _physics_process(delta):
+	var move_vec = Vector2()
+	var speed = 0
+	if Input.is_action_pressed("move_up_run"):
+		speed = 1
+	if Input.is_action_pressed("move_down_run"):
+		speed = -1
+	if Input.is_action_pressed("move_left_run"):
+		angle -= TURN_ANGLE
+	if Input.is_action_pressed("move_right_run"):
+		angle += TURN_ANGLE
+
+	move_vec = Vector2(speed * cos(angle), speed * sin(angle))
+	move_vec = move_vec.normalized()
+	move_and_collide(move_vec * MOVE_SPEED * delta)
+	global_rotation = angle
+
+func kill():
+	get_tree().reload_current_scene()
+
